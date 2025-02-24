081899ee4ab19bd9619e56bc6f2ca0d14220dbe9
fxboxed <dfg@boxedfx.com>
2025-02-24 11:56:05 +0000

    sorted country nav items alphabetically

--- public/js/countries.js
+++ public/js/countries.js
@@ -3,9 +3,8 @@ const countryLinks = document.querySelectorAll('.country-link');
 const dropdownButtons = document.querySelectorAll('.open-country-dropdown');
 const countryLists = document.querySelectorAll('.country-list');
 const bannerItems = document.querySelectorAll('.banner-items');
-
 window.onload = function() {
-    console.log('window loaded');
+
     // hide dropdown buttons if not on homepage
     if(window.location.href == 'http://localhost:1313/'  || window.location.href == 'http://localhost:1313/#navbar' || window.location.href == 'https://eugo.uk/' || window.location.href == 'https://eugo.uk/#navbar' || window.location.href == 'https://eugo.uk/#footer' )  {
         dropdownButtons.forEach(button => {
@@ -64,22 +63,6 @@ window.onload = function() {
             });
         });
     }
-
-
-//---------2nd dropdown menu for countries--------------
-// get 2nd dropdown btn
-// const showCountriesBtn = document.querySelectorAll('.show-countries-btn');
-
-// console.log('showCountriesBtn.length', showCountriesBtn.length);
-// if (showCountriesBtn.length > 0 && countryLists.length > 0) {
-//     console.log('showCountriesBtn.length', showCountriesBtn.length);
-//     showCountriesBtn.forEach(button => {
-//         button.addEventListener('click', () => {
-//             console.log('open clicked in btn 2');
-//             countryLists.forEach(list => {
-//                 list.classList.add('show-countries');
-
-//             });
-//         });
-//     });
-// }
+    //sort country links alphabetically
+    let sortedNavItems = Array.from(countryLinks).sort((a, b) => a.innerHTML.toLowerCase().localeCompare(b.innerHTML.toLowerCase())); // Sort items alphabetically
+    sortedNavItems.forEach(item => document.getElementById('topnav').appendChild(item));
